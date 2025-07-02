12675aa176dd58d5dc3b7ce839c04472372241bf
SimonMadudu <simon.madudu@gmail.com>
2025-07-02 11:36:27 -0500

    First commit
    create first draft

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your message!");
+});
+
+
+document.querySelector(".addition").addEventListener("click", add)
+document.querySelector(".subtraction").addEventListener("click", subtract)
+
+function add() {
+    let numberValue = parseInt(document.querySelector(".number").innerText)
+    numberValue += 1
+    document.querySelector(".number").innerText = numberValue
+}
+
+function subtract() {
+    let numberValue = parseInt(document.querySelector(".number").innerText)
+    if (numberValue > 0) {
+        numberValue -= 1
+    }
+
+    document.querySelector(".number").innerText = numberValue
+}
